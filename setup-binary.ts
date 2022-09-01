8c9a2146e317a4edb5fbc175e850d9a1f9dc7163
Jenna Goldstrich <jenna.goldstrich@hashicorp.com>
2022-09-01 13:31:03 -0700

    Add WIP logic to try and pull nightly releases for packer only right now, add axios to dev dependancies

--- setup-binary.ts
+++ setup-binary.ts
@@ -6,13 +6,17 @@ import * as sys from "./system";
 import cp from "child_process";
 import path from "path";
 import { ok } from "assert";
+import { download } from './utils'
 
-// TODO: cleanup
+const supportedBinaries = ["packer", "nomad", "nomad-pack"]
 
 export async function setupBinary(binaryName: string, version: string) {
   let userAgent = `setup-${binaryName} (GitHub Actions)`;
   let binaryPath = await fetchBinary(version, binaryName, userAgent);
 
+  if (!supportedBinaries.includes(binaryName)) {
+     throw new Error(`Binary ${binaryName} is not supported, only packer, nomad, and nomad-pack are supported`)
+  }
   core.info(`Adding ` + binaryName + ` to PATH.`);
   core.addPath(binaryPath);
 
@@ -34,46 +38,67 @@ async function fetchBinary(binaryName: string, version: string, userAgent: strin
   core.info(`binaryName ${binaryName}.`);
   core.info(`version ${version}.`);
   core.info(`userAgent ${userAgent}.`);
-  let release = await hc.getRelease(binaryName, version, userAgent);
-
   const nameAndVersion = binaryName + ` ` + version;
   const nameAndPlatform = binaryName + `_${osPlatform}`;
 
-  core.info(`Found ${nameAndVersion}.`);
+  if (version !== "nightly") {
+    let release = await hc.getRelease(binaryName, version, userAgent);
+    core.info(`Found ${nameAndVersion}.`);
 
-  core.info(`Checking cache for ${nameAndVersion}.`);
+    core.info(`Checking cache for ${nameAndVersion}.`);
 
-  core.debug(`Cache binary: ${nameAndPlatform}`);
-  binaryPath = cache.find(nameAndPlatform, version);
+    core.debug(`Cache binary: ${nameAndPlatform}`);
+    binaryPath = cache.find(nameAndPlatform, version);
 
-  if (binaryPath) {
-    core.info(`Found ${nameAndVersion} in cache at ${binaryPath}.`);
+    if (binaryPath) {
+      core.info(`Found ${nameAndVersion} in cache at ${binaryPath}.`);
+      return binaryPath;
+    }
+    core.info(`${nameAndVersion} not found in cache.`);
+    core.info(`Getting download URL for ${nameAndVersion}.`);
+    let build = release.getBuild(osPlatform, osArch);
+    core.debug(`Download URL: ${build.url}`);
+
+    core.info(`Downloading ${build.filename}.`);
+    let downloadPath = path.join(tmpDir, build.filename);
+
+    core.debug(`Download path: ${downloadPath}`);
+    await release.download(build.url, downloadPath, userAgent);
+
+    core.info(`Verifying ${build.filename}.`);
+    await release.verify(downloadPath, build.filename);
+
+    core.info(`Extracting ${build.filename}.`);
+    const extractedPath = await cache.extractZip(downloadPath);
+    core.debug(`Extracted path: ${extractedPath}`);
+
+    binaryPath = await cache.cacheDir(extractedPath, nameAndPlatform, version);
+    core.info(`Cached ${nameAndVersion} at ${binaryPath}.`);
+
     return binaryPath;
   }
+  else {
+    core.warning("Nightly releases are not official HashiCorp releases, but dev builds published by individual teams, use with caution")
 
-  core.info(`${nameAndVersion} not found in cache.`);
+    if (binaryName !== "packer") {
+      throw new Error("Nightly is only supported with packer")
+    }
 
-  core.info(`Getting download URL for ${nameAndVersion}.`);
-  let build = release.getBuild(osPlatform, osArch);
-  core.debug(`Download URL: ${build.url}`);
-
-  core.info(`Downloading ${build.filename}.`);
-  let downloadPath = path.join(tmpDir, build.filename);
-
-  core.debug(`Download path: ${downloadPath}`);
-  await release.download(build.url, downloadPath, userAgent);
-
-  core.info(`Verifying ${build.filename}.`);
-  await release.verify(downloadPath, build.filename);
-
-  core.info(`Extracting ${build.filename}.`);
-  const extractedPath = await cache.extractZip(downloadPath);
-  core.debug(`Extracted path: ${extractedPath}`);
-
-  binaryPath = await cache.cacheDir(extractedPath, nameAndPlatform, version);
-  core.info(`Cached ${nameAndVersion} at ${binaryPath}.`);
-
-  return binaryPath;
+    core.info(`Downloading nightly release info`);
+    let jsonPath = path.join(tmpDir, "metadata.json");
+    await download(`https://github.com/hashicorp/${binaryName}/releases/nightly/metadata.json`, jsonPath, userAgent)
+    const jsonData= require(path.join(tmpDir, "metadata.json"));
+    let nightlyVersion = jsonData.version
+    const nightlyFileName = binaryName + `_${nightlyVersion}_${osPlatform}.zip`
+    core.info(`Trying to download nightly release ${nightlyFileName}`);
+    let zipPath = path.join(tmpDir, nightlyFileName)
+    await download(`https://github.com/hashicorp/packer/releases/download/nightly/${nightlyFileName}`, zipPath, userAgent)
+    core.debug(`Extracting: ${zipPath}`);
+    const extractedPath = await cache.extractZip(zipPath);
+    core.debug(`Extracted path: ${extractedPath}`);
+   // We don't cache the result since nightly is not a stable build, and will change within the same dev version
+    return extractedPath;
+  }
 }
 
 export function parseVersion(version: string): string {
