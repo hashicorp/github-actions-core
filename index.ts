6f9e1b21fe97f1fde06981e00041dcc714536b14
Jenna Goldstrich <jenna.goldstrich2@gmail.com>
2022-11-22 09:12:50 -0800

    Working prototype PR (#3)
    Co-authored-by: Kerim Satirli <k.satirli@gmail.com>

---
+++ index.ts
@@ -0,0 +1,15 @@
+import * as core from "@actions/core";
+import {setupBinary} from "./setup-binary";
+
+export async function getHashicorpRelease(binary: string, version: string) {
+  core.info(`binary ${binary}`);
+  core.info(`version ${version}`);
+
+   try {
+     await setupBinary(binary, version);
+   } catch (error) {
+     if (error instanceof Error) {
+       core.setFailed(error.message);
+     }
+   }
+}
